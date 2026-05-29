2ee737b384b11ba44ebac159aa83a07b5500722c
Benedikt Weyer <bw.development@pm.me>
2026-05-29 15:37:38 +0200

    feat: refactor MainApplication to use ReactNativeHost for package management

--- apps/react-native/android/app/src/main/java/com/musestructnative/MainApplication.kt
+++ apps/react-native/android/app/src/main/java/com/musestructnative/MainApplication.kt
@@ -1,22 +1,31 @@
 package com.musestructnative
 
 import android.app.Application
+import com.facebook.react.BuildConfig
 import com.facebook.react.PackageList
 import com.facebook.react.ReactApplication
 import com.facebook.react.ReactHost
+import com.facebook.react.ReactNativeHost
 import com.facebook.react.ReactNativeApplicationEntryPoint.loadReactNative
 import com.facebook.react.defaults.DefaultReactHost.getDefaultReactHost
+import com.facebook.react.defaults.DefaultReactNativeHost
 
 class MainApplication : Application(), ReactApplication {
 
+  override val reactNativeHost: ReactNativeHost =
+      object : DefaultReactNativeHost(this) {
+        override fun getPackages() =
+            PackageList(this).packages.apply {
+              add(MusicFolderPackage())
+            }
+
+        override fun getJSMainModuleName(): String = "index"
+
+        override fun getUseDeveloperSupport(): Boolean = BuildConfig.DEBUG
+      }
+
   override val reactHost: ReactHost by lazy {
-    getDefaultReactHost(
-      context = applicationContext,
-      packageList =
-        PackageList(this).packages.apply {
-          add(MusicFolderPackage())
-        },
-    )
+    getDefaultReactHost(applicationContext, reactNativeHost)
   }
 
   override fun onCreate() {
